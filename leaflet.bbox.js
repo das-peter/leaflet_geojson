5b12d1b91e87d6950200472c6d500f49a76beda0
dasjo <dasjogibtsnet@gmail.com>
2012-11-23 20:36:33 +0100

    Cleanup.

--- leaflet.bbox.js
+++ leaflet.bbox.js
@@ -24,40 +24,33 @@
     makeGeoJSONLayer: function(map, url) {
       url = typeof url !== 'undefined' ? url : Drupal.settings.leafletBBox.url;
 
+      // Add bbox and zoom parameters as get params.
       url += "?bbox=" + map.getBounds().pad(0.05).toBBoxString();
       url += "&zoom=" + map.getZoom();
 
-      if (Drupal.leafletBBox._get['cluster_distance'] != undefined) {
-        url += "&cluster_distance=" + Drupal.leafletBBox._get['cluster_distance'];
-      }
-
       $.getJSON(url, function(data) {
         //New GeoJSON layer
-        var geojsonLayer = new L.GeoJSON(data, {
-          onEachFeature: function(featureData, layer) {
-            var popupText = featureData.properties.name;
-            layer.bindPopup(popupText);
-          },
-          pointToLayer: function(featureData, latlng) {
-            var icon = new L.NumberedDivIcon({number: featureData.cluster_items || 1});
-            lMarker = new L.Marker(latlng, {icon:icon});
-            return lMarker;
-          }
-        });
+        var geojsonLayer = new L.GeoJSON(data, Drupal.leafletBBox.geoJSONOptions);
         Drupal.leafletBBox.markerGroup.clearLayers();
         Drupal.leafletBBox.markerGroup.addLayer(geojsonLayer);
       });
+    }
+
+  };
+
+  Drupal.leafletBBox.geoJSONOptions = {
+
+    onEachFeature: function(featureData, layer) {
+      var popupText = featureData.properties.name;
+      layer.bindPopup(popupText);
     },
 
-    _get: new (function(){
-      var parts = window.location.search.substr(1).split("&");
-      var $_GET = {};
-      for (var i = 0; i < parts.length; i++) {
-        var temp = parts[i].split("=");
-        $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
-      }
-      return $_GET;
-    })
+    pointToLayer: function(featureData, latlng) {
+      var icon = new L.NumberedDivIcon({number: featureData.cluster_items || 1});
+      lMarker = new L.Marker(latlng, {icon:icon});
+      return lMarker;
+    }
+
   };
 
   // Inject into leaflet initialize.
