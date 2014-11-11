666356a57139f131cfe0ef10a1d1c5285cc41d2b
Peter Philipp <peter.philipp@cando-image.com>
2014-11-11 07:58:26 +0100

    Ensure multiple geojson layers can be used.

--- leaflet.bbox.js
+++ leaflet.bbox.js
@@ -2,31 +2,48 @@
 
   Drupal.leafletBBox = {
 
-    map: null,
-    markerGroup: null,
+    // Stores the instances of bbox handling.
+    instances: [],
 
-    onMapLoad: function(map) {
-      Drupal.leafletBBox.map = map;
+    onMapLoad: function(leaflet_map_id, map) {
 
-      Drupal.leafletBBox.markerGroup = new L.LayerGroup();
-      Drupal.leafletBBox.markerGroup.addTo(map);
+      if (typeof(Drupal.settings.leafletBBox[leaflet_map_id]) != 'undefined') {
 
-      map.on('moveend', Drupal.leafletBBox.moveEnd);
-      Drupal.leafletBBox.moveEnd();
-    },
+        Drupal.leafletBBox.instances[leaflet_map_id] = {
+          map: map,
+          markerGroup: null
+        };
 
-    moveEnd: function(e) {
-      var map = Drupal.leafletBBox.map;
-      if (!map._popup) {
-        Drupal.leafletBBox.makeGeoJSONLayer(map);
+        map.eachLayer(function (layer) {
+          if (layer._leaflet_id == Drupal.settings.leafletBBox[leaflet_map_id].leaflet_layer_id) {
+            Drupal.leafletBBox.instances[leaflet_map_id].markerGroup = layer;
+          }
+        });
+
+        if (Drupal.leafletBBox.instances[leaflet_map_id].markerGroup == null) {
+          Drupal.leafletBBox.instances[leaflet_map_id].markerGroup = new L.LayerGroup();
+          Drupal.leafletBBox.instances[leaflet_map_id].markerGroup.addTo(map);
+          Drupal.leafletBBox.moveEnd();
+        }
+        map.on('moveend', function(e) { Drupal.leafletBBox.moveEnd(e, leaflet_map_id) });
       }
     },
 
-    makeGeoJSONLayer: function(map, url) {
-      url = typeof url !== 'undefined' ? url : Drupal.settings.leafletBBox.url;
+    moveEnd: function(e, leaflet_map_id) {
+      var map = Drupal.leafletBBox.instances[leaflet_map_id].map;
+      if (!map._popup) {
+        Drupal.leafletBBox.makeGeoJSONLayer(leaflet_map_id);
+      }
+    },
 
-      var bbox_arg_id = ('bbox_arg_id' in Drupal.settings.leafletBBox) ?
-        Drupal.settings.leafletBBox.bbox_arg_id : 'bbox';
+    makeGeoJSONLayer: function(leaflet_map_id, url) {
+      var instance = Drupal.leafletBBox.instances[leaflet_map_id];
+      var map = instance.map;
+      var settings = Drupal.settings.leafletBBox[leaflet_map_id];
+      url = typeof url !== 'undefined' ? url : settings.url;
+
+      var bbox_arg_id = ('bbox_arg_id' in settings) ?
+        settings.bbox_arg_id : 'bbox';
 
       // Add bbox and zoom parameters as get params.
       url += "?" + bbox_arg_id +"=" + map.getBounds().toBBoxString();
@@ -35,8 +52,8 @@
       $.getJSON(url, function(data) {
         //New GeoJSON layer
         var geojsonLayer = new L.GeoJSON(data, Drupal.leafletBBox.geoJSONOptions);
-        Drupal.leafletBBox.markerGroup.clearLayers();
-        Drupal.leafletBBox.markerGroup.addLayer(geojsonLayer);
+        instance.markerGroup.clearLayers();
+        instance.markerGroup.addLayer(geojsonLayer);
       });
     }
 
@@ -57,7 +74,7 @@
   };
 
   $(document).bind('leaflet.map', function(e, map, lMap){
-    Drupal.leafletBBox.onMapLoad(lMap);
+    Drupal.leafletBBox.onMapLoad(map.map_id, lMap);
   });
 
 })(jQuery);
--- leaflet_geojson.api.php
+++ leaflet_geojson.api.php
@@ -20,12 +20,17 @@
  * @return array
  *   An associative array of sources, keyed by a unique
  *   identifier and containing associative arrays with the following keys:
+ *   - leaflet_map_id: The id of the map this source belongs to.
  *   - title: A human readable title for the source.
  *   - url: The GeoJSON source url.
  *   - bbox: (optional) Set to TRUE to activate the Bounding Box strategy.
  *   - bbox_arg_id: (optional) Specify the bbox argument identifier.
  *       Defaults to 'bbox'.
  *   - type: (optional) The type of source, for example views_geojson.
+ *   - group_id: (optional) Id of the layer group the features are assigned to.
+ *   - group_label: (optional) Label of the group for e.g. the layer control.
+ *   - group_layer_control: (optional) If enabled the layer control of the map
+ *     is enabled.
  *   - ... (optional) further parameters specific to the source type.
  */
 function hook_leaflet_geojson_source_info() {
@@ -35,13 +40,17 @@ function hook_leaflet_geojson_source_info() {
     'url' => 'http://example.com',
   );
   $sources['views_geojson_source'] = array(
+    'leaflet_map_id' => 'points_of_interest',
     'title' => 'My Source',
     'url' => 'http://example.com',
     'bbox' => TRUE,
     'bbox_arg_id' => 'bbox',
     'type' => 'views_geojson',
     'view' => 'view_name',
-    'view_display' => 'view_display_name'
+    'view_display' => 'view_display_name',
+    'group_id' => 'views_geojson_source_group_layer',
+    'group_label' => t('Points of interest'),
+    'group_layer_control' => TRUE,
   );
   return $sources;
 }
@@ -56,4 +65,16 @@ function hook_leaflet_geojson_source_info() {
  */
 function hook_leaflet_geojson_source_info_alter(array &$infos) {
   $infos['simple_source']['url'] = 'http://somewhere.else';
+}
+
+/**
+ * Alter the Leaflet GeoJSON settings just before they are used.
+ *
+ * @param array $settings
+ *   The source info array, keyed by source identifier.
+ *
+ * @see hook_leaflet_geojson_source_info()
+ */
+function leaflet_geojson_add_bbox_strategy_alter(array &$settings) {
+  // Add your own JS or adjust settings.
 }
